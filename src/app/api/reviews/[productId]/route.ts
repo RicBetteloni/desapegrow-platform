e0a2c9308630b3dce262d6db1e3cd16a204bb913
RBL <ricardobl6@msn.com>
2025-09-24 02:47:35 -0300

    Fix: route7

--- src/app/api/reviews/[productId]/route.ts
+++ src/app/api/reviews/[productId]/route.ts
@@ -1,6 +1,5 @@
-// src/app/api/reviews/[productId]/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
+import { prisma } from '../../../../lib/prisma';
 
 export async function GET(
   request: NextRequest,
